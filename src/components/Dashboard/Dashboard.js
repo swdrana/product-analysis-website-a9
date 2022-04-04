44fb11d591372be43843e3ed36f4e2718768fd9a
swdrana <tctr2s@gmail.com>
2022-04-04 14:21:17 +0600

    more 2 total 4 chart added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -7,6 +7,13 @@ import {
   Legend,
   Line,
   LineChart,
+  Pie,
+  PieChart,
+  PolarAngleAxis,
+  PolarGrid,
+  PolarRadiusAxis,
+  Radar,
+  RadarChart,
   Tooltip,
   XAxis,
   YAxis,
@@ -20,10 +27,10 @@ const Dashboard = () => {
       .then((data) => setData(data));
   }, []);
   return (
-    <div className="p-5 d-flex justify-content-evenly">
+    <div className="row d-flex justify-content-evenly">
       {/* Chart 1 */}
 
-      <LineChart width={500} height={250} data={data}>
+      <LineChart width={500} height={250} data={data} className="col-md-6">
         <CartesianGrid strokeDasharray="5" />
         <XAxis dataKey="month" />
         <YAxis dataKey="investment" />
@@ -35,7 +42,7 @@ const Dashboard = () => {
 
       {/* Chart 2  */}
 
-      <ComposedChart width={500} height={250} data={data}>
+      <ComposedChart width={500} height={250} data={data} className="col-md-6">
         <XAxis dataKey="month" />
         <YAxis />
         <Tooltip />
@@ -50,6 +57,62 @@ const Dashboard = () => {
         <Bar dataKey="sell" barSize={20} fill="#413ea0" />
         <Line type="monotone" dataKey="revenue" stroke="#ff7300" />
       </ComposedChart>
+
+      {/* Chart 3 */}
+
+      <PieChart width={500} height={250} className="col-md-6">
+        <Pie
+          data={data}
+          dataKey="sell"
+          nameKey="month"
+          cx="50%"
+          cy="50%"
+          outerRadius={50}
+          fill="rgb(65, 62, 160)"
+        />
+        <Pie
+          data={data}
+          dataKey="investment"
+          nameKey="revenue"
+          innerRadius={60}
+          outerRadius={80}
+          fill="orange"
+          label
+        />
+        <Tooltip />
+        <Legend />
+      </PieChart>
+
+      {/* Chart 4  */}
+
+      <RadarChart outerRadius={90} width={500} height={250} data={data}>
+        <PolarGrid />
+        <PolarAngleAxis dataKey="month" />
+        <PolarRadiusAxis angle={30} domain={[0, 100]} />
+        <Radar
+          name="Investment"
+          dataKey="investment"
+          stroke="orange"
+          fill="orange"
+          fillOpacity={0.6}
+        />
+        <Radar
+          name="Revenue"
+          dataKey="revenue"
+          stroke="orangered"
+          fill="orangered"
+          fillOpacity={0.6}
+        />
+        <Radar
+          name="Sell"
+          dataKey="sell"
+          stroke="rgb(65, 62, 160)"
+          fill="rgb(65, 62, 160)"
+          fillOpacity={0.7}
+        />
+        <Legend />
+        <Tooltip />
+      </RadarChart>
     </div>
   );
 };
