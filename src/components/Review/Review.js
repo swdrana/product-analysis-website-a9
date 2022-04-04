7cb472560fb93d031b068d0a337c1697a7a5776b
swdrana <tctr2s@gmail.com>
2022-04-04 10:52:23 +0600

    review item added

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './Review.css'
 const Review = () => {
     return (
-        <div>
-            d
+        <div className='single-review p-3'>
+            <h5 className='text-start'>Rana</h5>
+            <p className='text-start'>Reating: 4.5 </p>
+            <p className='text-start text-success'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet quaerat qui dignissimos tempore fugiat laborum voluptatem facere, in alias ex quisquam ad iusto! Adipisci minima deserunt sit repudiandae veritatis repellendus harum hic nemo fugiat praesentium esse, vero cupiditate temporibus velit.</p>
         </div>
     );
 };
