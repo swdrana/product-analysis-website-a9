cd427af453a6f47607f7e01d6e250536f9185b5f
swdrana <tctr2s@gmail.com>
2022-04-04 10:42:29 +0600

    home page area done

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Review from '../Review/Review';
 import './Reviews.css'
 const Reviews = () => {
     return (
         <div>
-            
+            <Review></Review>
         </div>
     );
 };
